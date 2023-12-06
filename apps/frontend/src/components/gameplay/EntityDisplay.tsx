b293995ab023a8a24eed63b981c3cae8b817cede
bradyz314 <bradyzhou314159@gmail.com>
2023-12-06 01:29:01 -0500

    Remove the use of EntityData

--- apps/frontend/src/components/gameplay/EntityDisplay.tsx
+++ apps/frontend/src/components/gameplay/EntityDisplay.tsx
@@ -1,20 +1,20 @@
-import { EntityData } from "../../types";
 import InfoBar from "./InfoBar";
 
 interface EntityDisplayProps {
     row: string,
     col: string,
-    data: EntityData
+    player: boolean,
+    imgPath: string,
 }
 
-export default function EntityDisplay({row, col, data} : EntityDisplayProps) {
+export default function EntityDisplay({ row, col, player, imgPath }: EntityDisplayProps) {
     return (
-        <div 
+        <div
             className={`${row} ${col} justify-self-center`}
         >
-            <InfoBar data={data} player={data.isPlayer}/>
-            <img 
-                src={data.imgPath}
+            <InfoBar player={player} />
+            <img
+                src={imgPath}
                 className='mx-auto'
             />
         </div>
