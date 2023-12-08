036cc281c2fad301d03ebb1a7f2ac14313134836
bradyz314 <bradyzhou314159@gmail.com>
2023-12-08 04:11:45 -0500

    Update screens

--- apps/frontend/src/components/screens/MainScreen.tsx
+++ apps/frontend/src/components/screens/MainScreen.tsx
@@ -1,90 +1,50 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
-import AccountMenu from "../menus/AccountMenu";
+import axios from "axios"
+import LevelSelectionMenu from "../menus/LevelSelectionMenu";
 import { useDispatch } from "react-redux";
 import { setState } from "../../features/GameStateSlice";
 
+interface LevelNum {
+    levelNo: number,
+}
+
 export default function MainScreen() {
-    const [show, setShow] = useState(false);
-    const [login, setLogin] = useState(false);
-    const [user, setUser] = useState('');
     const dispatch = useDispatch();
+    const [levels, setLevels] = useState<number[]>([]);
+    const [maxUnlocked, setMaxUnlocked] = useState(1);
     useEffect(() => {
-        axios.get('/account/user')
-            .then(res => {
-                if (!res.data) {
-                    setUser('')
-                } else {
-                    setUser(res.data)
-                }
-            }).catch((err) => {
-                console.log(err);
-            })
-    }, [user]);
+        axios.get('/level/getSortedLevelNumbers').then(levelsRes => {
+            setLevels(levelsRes.data.map((level: LevelNum) => level.levelNo));
+        });
+        axios.get('/account/currentData').then((dataRes) => {
+            setMaxUnlocked(dataRes.data.maxLevelUnlocked);
+        })
+    }, []); 
     return (
         <div
-            className='min-h-full flex-col flex items-center justify-center'
+            className='flex flex-col min-h-full relative'
         >
-            <h1
-                className='text-9xl flex bg-gradient-to-r from-amber-300 via-cyan-300 to-rose-300 text-transparent bg-clip-text'
+            <button
+                className='rounded-md w-[5%] text-4xl text-white rounded-s m-10 hover:text-rose-400'
+                onClick={() => dispatch(setState('TITLE'))}
             >
-                Animal Mayhem
-            </h1>
-            {user ?
-                <>
-                    <h2
-                        className='text-4xl text-white my-2'
-                    >
-                        {`Welcome ${user}!`}
-                    </h2>
-                    <button
-                        className='rounded-md w-1/4 bg-blue-600 text-xl text-emerald-400 rounded-s m-2 hover:bg-blue-700'
-                        onClick={() => {
-                            dispatch(setState('GAME'));
-                        }}
-                    >
-                        Play
-                    </button>
-                    <button
-                        className='rounded-md w-1/4 bg-blue-600 text-xl text-emerald-400 rounded-s m-2 hover:bg-blue-700'
-                        onClick={() => {
-                            axios.post(`/account/logout`).then(() => {
-                                setUser('');
-                            }).catch((err) => {
-                                alert(`${err.response.data}!`);
-                            })
-                        }}
-                    >
-                        Log Out
-                    </button>
-                </> :
-                <>
-                    <h2
-                        className='text-4xl text-white my-2'
-                    >
-                        Please sign up and/or log in!
-                    </h2>
-                    <button
-                        className='rounded-md w-1/4 bg-blue-600 text-xl text-emerald-400 rounded-s m-2 hover:bg-blue-700'
-                        onClick={() => {
-                            setLogin(true);
-                            setShow(true);
-                        }}
-                    >
-                        Login
-                    </button>
-                    <button
-                        className='rounded-md w-1/4 bg-blue-600 text-xl text-emerald-400 rounded-s m-2 hover:bg-blue-700'
-                        onClick={() => {
-                            setLogin(false);
-                            setShow(true);
-                        }}
-                    >
-                        Sign Up
-                    </button>
-                </>
-            }
-            {show && <AccountMenu login={login} setShow={setShow} setUser={setUser} />}
+                X
+            </button>
+            <LevelSelectionMenu levels={levels} maxUnlocked={maxUnlocked}/>
+            <div
+                className='absolute top-[85%] left-[88%]'
+            >
+                <button
+                    className='text-white text-3xl absolute top-1/4 left-0 hover:text-red-400'
+                    onClick={() => dispatch(setState('UPGRADE'))}
+                >
+                    UPGRADES
+                </button>
+                <img
+                    src='/crossed-swords.png'
+                    className='mx-auto'
+                />
+            </div>
         </div>
-    )
+    )   
 }
---
+++ apps/frontend/src/components/screens/TitleScreen.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import AccountMenu from "../menus/AccountMenu";
+import { useDispatch } from "react-redux";
+import { setState } from "../../features/GameStateSlice";
+
+export default function TitleScreen() {
+    const [show, setShow] = useState(false);
+    const [login, setLogin] = useState(false);
+    const [user, setUser] = useState('');
+    const dispatch = useDispatch();
+    useEffect(() => {
+        axios.get('/account/user')
+            .then(res => {
+                if (!res.data) {
+                    setUser('')
+                } else {
+                    setUser(res.data)
+                }
+            }).catch((err) => {
+                console.log(err);
+            })
+    }, [user]);
+    return (
+        <div
+            className='min-h-full flex-col flex items-center justify-center'
+        >
+            <h1
+                className='text-9xl flex bg-gradient-to-r from-amber-300 via-cyan-300 to-rose-300 text-transparent bg-clip-text'
+            >
+                Animal Mayhem
+            </h1>
+            {user ?
+                <>
+                    <h2
+                        className='text-4xl text-white my-2'
+                    >
+                        {`Welcome ${user}!`}
+                    </h2>
+                    <button
+                        className='rounded-md w-1/4 bg-blue-600 text-xl text-emerald-400 rounded-s m-2 hover:bg-blue-700'
+                        onClick={() => {
+                            dispatch(setState('MAIN MENU'));
+                        }}
+                    >
+                        Play
+                    </button>
+                    <button
+                        className='rounded-md w-1/4 bg-blue-600 text-xl text-emerald-400 rounded-s m-2 hover:bg-blue-700'
+                        onClick={() => {
+                            axios.post(`/account/logout`).then(() => {
+                                setUser('');
+                            }).catch((err) => {
+                                alert(`${err.response.data}!`);
+                            })
+                        }}
+                    >
+                        Log Out
+                    </button>
+                </> :
+                <>
+                    <h2
+                        className='text-4xl text-white my-2'
+                    >
+                        Please sign up and/or log in!
+                    </h2>
+                    <button
+                        className='rounded-md w-1/4 bg-blue-600 text-xl text-emerald-400 rounded-s m-2 hover:bg-blue-700'
+                        onClick={() => {
+                            setLogin(true);
+                            setShow(true);
+                        }}
+                    >
+                        Login
+                    </button>
+                    <button
+                        className='rounded-md w-1/4 bg-blue-600 text-xl text-emerald-400 rounded-s m-2 hover:bg-blue-700'
+                        onClick={() => {
+                            setLogin(false);
+                            setShow(true);
+                        }}
+                    >
+                        Sign Up
+                    </button>
+                </>
+            }
+            {show && <AccountMenu login={login} setShow={setShow} setUser={setUser} />}
+        </div>
+    )
+}
