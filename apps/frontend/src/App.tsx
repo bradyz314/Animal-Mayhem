db418ed6ca392de10ebb56e087effc8ab3ac82e3
bradyz314 <bradyzhou314159@gmail.com>
2023-12-08 04:09:55 -0500

    Add state for upgrade screen

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import './App.css'
 import '@fontsource/vt323';
 import GameScreen from './components/screens/GameScreen';
-import MainScreen from './components/screens/MainScreen';
+import TitleScreen from './components/screens/TitleScreen';
 import { useSelector } from 'react-redux';
 import { RootState } from './app/store';
+import MainScreen from './components/screens/MainScreen';
+import UpgradeScreen from './components/screens/UpgradeScreen';
 
 export default function App() {
-  const {state, levelNo} = useSelector((state: RootState) => state.gameState);
+  const state = useSelector((state: RootState) => state.gameState.state);
   return (
     <>
-      {state === 'MAIN' && <MainScreen />}
-      {state === 'GAME' && <GameScreen levelNo={levelNo}/>}
+      {state === 'TITLE' && <TitleScreen />}
+      {state === 'MAIN MENU' && <MainScreen />}
+      {state === 'GAME' && <GameScreen />}
+      {state === 'UPGRADE' && <UpgradeScreen />}
     </>
   )
 }
